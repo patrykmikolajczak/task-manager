b551ef2564fc03f00ac714cea0d25c022714a10d
Patryk Mikołajczak <krawlof@gmail.com>
2025-05-14 17:39:24 +0200

    Add Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('TaskManager API')
+    .setDescription('API do zarządzania zadaniami')
+    .setVersion('1.0')
+    .addBearerAuth() // jeśli używasz JWT
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
